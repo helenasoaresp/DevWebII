b7a4b1c0e2f6b6a909cfba77ce4ab32dbf743235
helenasoaresp <helenasoarees23@gmail.com>
2025-03-31 11:52:04 -0300

    feat: Resolução de exemplo e iniciando exercício

---
+++ aula05-renderizacaoTemplates/src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+  const nome = 'João';
+  const idade = '25'
+
+  function inverter(texto) {
+    return texto.split('').reverse().join('');
+  }
+
+  function saudacao() {
+      return `Olá, ${nome}!`; 
+  }
+
+</script>
+
+<template>
+  
+  <div class="info">
+    <h1>Exemplo 1 - Mostrando informações na tela</h1>
+    <p>Nome: {{ nome }}</p>
+    <p>Idade: {{ idade }}</p>
+    <hr />
+    <p>Para completar 50 anos faltam: {{ 50 - idade }}  anos</p>
+    <p>O nome tem {{ nome.length }}  caracteres</p>
+    <p>O nome invertido é:  {{ inverter(nome) }}</p>
+    <hr />
+    <p>Exemplo de saudadação: usando função</p>
+    <p> {{ saudacao() }} </p>
+  </div>
+
+</template>
+
+<style scoped>
+
+  .info {
+    background-color: rgb(35,12,22,1);
+    padding: 20px 30px;
+    color: rgb(183, 210, 219);
+    border-radius: 12px;
+  }
+
+  h1 {
+    color: white;
+    font-weight: bold;
+    font-size: 1.5rem;
+  }
+
+</style>
