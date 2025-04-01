ec2e6fc6bd80cd786fdb7735e0732488c7dd6ffd
helenasoaresp <helenasoarees23@gmail.com>
2025-04-01 09:29:32 -0300

    Aula 06 - Propriedades Computadas

---
+++ aula06-exemploPropriedadesComputadas/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+  import { reactive, computed } from 'vue';
+
+  const professor = reactive({
+    nome: 'Eduardo da Silva',
+    disciplinas: [
+      'Desenvolvimento Web II',
+      'Desenvolvimento de Aplicativos para Dispositivos Móveis' ,
+    ],
+  });
+
+  const possuiDisciplinas = computed(() => {
+    return professor.disciplinas.length > 0;
+  })
+</script>
+
+<template>
+
+  <div>
+    <h1>Professor</h1>
+    <p>Nome: {{ professor.nome }}</p>
+    <p v-if="!possuiDisciplinas">O professor não possui disciplinas.</p>
+    
+    <div v-else>
+      <p>O professor possui disciplinas.</p>
+    </div>
+
+    <ul>
+        <li v-for="disciplina in professor.disciplinas" :key="disciplina">
+          {{ disciplina }}
+        </li>
+      </ul>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ aula06-exerciciosPropriedadesComputadas/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import { reactive, computed } from 'vue';
+
+  const contador1 = reactive({
+    
+  })
+</script>
+
+<template>
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+
+</style>
