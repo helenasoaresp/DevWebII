1a5c9250f422df8c02278e1a196f88c4b2153cc6
helenasoaresp <helenasoarees23@gmail.com>
2025-04-01 08:59:54 -0300

    Corrigindo exercício

--- aula05-exercicio/src/App.vue
+++ aula05-exercicio/src/App.vue
@@ -1,15 +1,27 @@
 <script setup>
+  import { ref } from 'vue';
 
-  const nome = 'Helena';
-  const idade = '16'
+  const nome = ref('Helena');
+  const idade = ref('16');
+  const contador = ref(0);
 
   function inverter(texto) {
-    return texto.split('').reverse().join('');
-  }
+  return texto.split('').reverse().join('').toUpperCase()
+}
 
-  function saudacao() {
-      return `Olá, ${nome}!`; 
-  }
+  function incrementar() {
+  contador.value++
+}
+
+  function decrementar() {
+  if (contador.value > 0) {
+      contador.value--;
+    }
+}
+
+function reiniciar() {
+  contador.value = 0;
+}
 
 </script>
 
@@ -18,31 +30,67 @@
   <div class="info">
     <h1>Exemplo 1 - Mostrando informações na tela</h1>
     <p>Nome: {{ nome }}</p>
-    <p>Idade: {{ idade }}</p>
+    <p>Idade: {{ idade }} anos</p>
     <hr />
-    <p>Para completar 50 anos faltam: {{ 50 - idade }}  anos</p>
-    <p>O nome tem {{ nome.length }}  caracteres</p>
-    <p>O nome invertido é:  {{ inverter(nome) }}</p>
+    <p>Para completar 50 anos faltam: {{ 50 - idade }} anos</p>
+    <p>O nome tem {{ nome.length }} caracteres</p>
+    <p>O nome invertido é: {{ inverter(nome) }}</p>
     <hr />
-    <p>Exemplo de saudadação: usando função</p>
-    <p> {{ saudacao() }} </p>
+
+  </div>
+  <div class="contador">
+    <h2>Contador</h2>
+    <p>Valor: {{ contador }} </p>
+    <button @click="incrementar">Incrementar</button>
+    <button @click="decrementar">Decrementar</button>
+    <button @click="reiniciar">Reiniciar</button>
+    <div
+      :style="{ backgroundColor: contador > 10 ? 'green' : 'red' }"
+    >
+      <p v-if="contador > 10">Valor maior que 10</p>
+      <p v-else>Valor menor que 10</p>
+    </div>
   </div>
 
 </template>
 
 <style scoped>
 
-  .info {
-    background-color: rgb(35,12,22,1);
+.info,
+  .contador {
+    background-color: rgba(35, 12, 22, 1);
     padding: 20px 30px;
     color: rgb(183, 210, 219);
-    border-radius: 12px;
+    border-radius: 10px;
   }
 
   h1 {
+    color: #fff;
+    font-weight: bold;
+    font-size: 1.5em;
+  }
+
+  button {
+    margin: 10px;
+    padding: 10px;
+    border: 0;
+    border-radius: 5px;
+    background-color: lightblue;
+    color: black;
+    font-weight: bold;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: blue;
+  }
+
+  div {
+    margin: 10px 0;
+    padding: 10px;
+    border-radius: 5px;
     color: white;
     font-weight: bold;
-    font-size: 1.5rem;
   }
 
 </style>
