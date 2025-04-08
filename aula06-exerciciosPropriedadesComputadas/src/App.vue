e9a90bc8e232c9982ffcec5d67f1e4ccc949094f
helenasoaresp <helenasoarees23@gmail.com>
2025-04-08 08:30:48 -0300

    Exercício finalizado

--- aula06-exerciciosPropriedadesComputadas/src/App.vue
+++ aula06-exerciciosPropriedadesComputadas/src/App.vue
@@ -32,8 +32,16 @@
 
   <div v-if="booleano">
     <div class="contador">
+      <h2>Contador 1</h2>
       <button @click="incrementar('contador1')">Incrementar</button>
       <button @click="decrementar('contador1')">Decrementar</button>
+      <p>Valor: </p>
+    </div>
+
+    <div class="contador">
+      <h2>Contador 2</h2>
+      <button @click="incrementar('contador2')">Incrementar</button>
+      <button @click="decrementar('contador2')">Decrementar</button>
       <p>Valor: </p>
     </div>
 
@@ -55,17 +63,28 @@
   </div>
   <div v-else>
     <p>A soma é impar</p>
+
   </div>
 
 </template>
 
 <style scoped>
 
+  h2 {
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    color: rgb(51, 53, 0);
+  }
+
+  p {
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  }
+
   button {
-    background-color: rgb(215, 145, 255);
+    border: none;
+    background-color: rgba(242, 255, 128, 0.411);
     border-radius: 5px;
-    padding: 5px;
-    margin: 8px;
+    padding: 8px;
+    margin: 3px;
   }
 
 </style>
