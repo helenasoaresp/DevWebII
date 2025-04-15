1b2847f08b872e01b67294b17153690f29b4d216
helenasoaresp <helenasoarees23@gmail.com>
2025-04-15 09:32:30 -0300

    FEAT: Iniciando exercício

---
+++ exercicio-carrinhoDeCompras/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+//import { computed, reactive } from "vue";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <div class="logo">
+        <a href="#">IFbooks</a>
+        <p>Apreço a leitura</p>
+      </div>
+
+      
+    </nav>
+  </header>
+</template>
+
+<style scoped></style>
